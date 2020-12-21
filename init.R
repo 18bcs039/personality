b74f3b2fae604b098372c21ded0294f7f987a5a0
18bcs039 <76406665+18bcs039@users.noreply.github.com>
2020-12-21 08:30:00 +0530

    Update init.R

--- init.R
+++ init.R
@@ -1,18 +1,4 @@
-my_packages = c("shiny", 
-                "shinythemes", 
-                "knitr", 
-                "shinyWidgets", 
-                "data.table", 
-                "ggplot2", 
-                "caret", 
-                "RCurl", 
-                "FNN", 
-                "class", 
-                "gmodels", 
-                "splitstackshape", 
-                "tidyverse", 
-                "RDS", 
-                "packrat")
+my_packages = c("shiny", "shinythemes", "knitr", "shinyWidgets", "data.table", "ggplot2", "caret", "RCurl", "FNN", "class", "gmodels", "splitstackshape", "tidyverse", "RDS", "packrat")
 
 install_if_missing = function(k) {
     if (k %in% rownames(installed.packages()) == FALSE) {
